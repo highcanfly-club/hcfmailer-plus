a8cec91368c03fef66ef57fbcaddc78b467d5f29
Ronan <ronan.le_meillat@sctg.eu.org>
2023-02-22 07:24:24 +0000

    multi proxy

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,7 +1,8 @@
 import type {Request} from "@cloudflare/workers-types"
 export interface Env {
 	OKTETO_FQDN_HCFMAILER: string,
-	OKTETO_FQDN_MAILTRAIN: string
+	OKTETO_FQDN_MAILTRAIN_PUBLIC: string
+	OKTETO_FQDN_MAILTRAIN_TRUSTED: string
 }
 
 export default {
@@ -11,9 +12,16 @@ export default {
 		ctx: ExecutionContext
 	): Promise<Response> {
 		const url = new URL(request.url);
-		request.url.toLowerCase().includes('newsletter') ?
-			url.hostname = env.OKTETO_FQDN_HCFMAILER :
-			url.hostname = env.OKTETO_FQDN_MAILTRAIN ;
+		const _rurl = request.url.toLowerCase();
+		if (_rurl.includes('newsletter')){
+			url.hostname = env.OKTETO_FQDN_HCFMAILER
+		} else if (_rurl.includes('list')){
+			url.hostname = env.OKTETO_FQDN_MAILTRAIN_PUBLIC ;
+		} else if (_rurl.includes('mailtrain')){
+			url.hostname = env.OKTETO_FQDN_MAILTRAIN_TRUSTED ;
+		} else {
+			url.hostname = env.OKTETO_FQDN_MAILTRAIN_PUBLIC 
+		}
 		console.log(`Proxy to:${request.method} ${url.toString()}`)
 		request.headers.forEach((value,key) => {
 			console.log(`\t${key}:${value}`)
