c0898623ede01c808c9c2d92f3a8c4a327273b20
Ronan LE MEILLAT <rtkgps@outlook.com>
2023-05-09 11:23:32 +0200

    up

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 TAG=$1
 echo $TAG
 sed -i .bak "s/^appVersion: .*/appVersion: \"$TAG\"/g" ./k8s/helm/hcfmailerplus/Chart.yaml
-#git commit -m "Update chart appVersion to $TAG"
-#git tag
-#git push --tags && gh --repo highcanfly-club/helm-dashboard release create $TAG --notes "build $TAG"
+git commit -m "Update chart appVersion to $TAG"
+git tag
+git push --tags && gh --repo highcanfly-club/helm-dashboard release create $TAG --notes "build $TAG"
