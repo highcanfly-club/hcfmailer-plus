f59b54e29b1b8912dd3407865274b16f1e611ac6
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2023-05-10 20:34:10 +0200

    sample kaniko build

--- kaniko-build.sh
+++ kaniko-build.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+# Tiltfile looks like:
+# os. putenv ( 'DOCKER_USERNAME' , 'registry-user' ) 
+# os. putenv ( 'DOCKER_PASSWORD' , 'password' ) 
+# os. putenv ( 'DOCKER_EMAIL' , 'none@example.org' ) 
+# os. putenv ( 'DOCKER_REGISTRY' , 'registry.example.org' ) 
+# Namespace='sandbox-hcfp'
+# os.putenv('NAMESPACE',Namespace)
+# allow_k8s_contexts('kubernetes-admin')
+# k8s_yaml(helm('./k8s/helm/hcfmailerplus', name='hcfmailer-plus', namespace=Namespace, values='dev-values.yaml'))
+# custom_build('highcanfly/hcfmailer-plus','./kaniko-build.sh',['./autocert', './scripts'],skips_local_docker=True)
+
+
 #kubectl create secret generic registry-credentials --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
-
-
-kubectl create secret docker-registry registry-credentials --docker-server=$DOCKER_REGISTRY --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
-tar -cv --exclude "node_modules" --exclude ".git" --exclude ".github" --exclude-vcs --exclude ".docker" -f -  . | gzip -9 | kubectl run -n sandbox-hcfp kaniko \
+#   EXPECTED_REF=serveur/highcanfly_hcfmailer-plus:tilt-build-1683738819
+#   EXPECTED_IMAGE=highcanfly_hcfmailer-plus
+#   EXPECTED_TAG=tilt-build-1683738819
+#   REGISTRY_HOST=server.fqdn
+#   EXPECTED_REGISTRY=server.fqdn
+kubectl create namespace $NAMESPACE
+kubectl create secret -n $NAMESPACE docker-registry registry-credentials --docker-server=$EXPECTED_REGISTRY --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
+tar -cv --exclude "node_modules" --exclude ".git" --exclude ".github" --exclude-vcs --exclude ".docker" -f -  . | gzip -9 | kubectl run -n $NAMESPACE kaniko \
     --rm --stdin=true \
     --image=gcr.io/kaniko-project/executor:latest --restart=Never \
     --overrides='{
@@ -20,7 +36,7 @@ tar -cv --exclude "node_modules" --exclude ".git" --exclude ".github" --exclude-
           "--dockerfile=Dockerfile",
           "--context=tar://stdin",
           "--skip-tls-verify",
-          "--destination='$REFERENCE'"
+          "--destination='$EXPECTED_REF'"
         ],
         "volumeMounts": [
           {
@@ -47,4 +63,5 @@ tar -cv --exclude "node_modules" --exclude ".git" --exclude ".github" --exclude-
     "restartPolicy": "Never"
   }
 }'
-kubectl delete secret registry-credentials
+
+#kubectl delete -n $NAMESPACE secret/registry-credentials
