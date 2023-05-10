f53dc661ed8c5b633a4904e4b6ec9b7900147b94
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2023-05-10 08:52:51 +0200

    bug

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@ TAG=$1
 echo $TAG
 sed -i .bak "s/^appVersion: .*/appVersion: \"$TAG\"/g" ./k8s/helm/hcfmailerplus/Chart.yaml
 git commit -m "Update chart appVersion to $TAG" ./k8s/helm/hcfmailerplus/Chart.yaml
-git tag
+git tag $TAG 
 git push --tags && gh --repo highcanfly-club/hcfmailer-plus release create $TAG --notes "build $TAG"
