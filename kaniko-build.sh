528a7e9096c8569411c276cfd35d2c27ade9300a
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2023-05-11 11:48:39 +0200

    v20230511.1

--- kaniko-build.sh
+++ kaniko-build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Tiltfile looks like:
-# os. putenv ( 'DOCKER_USERNAME' , 'registry-user' ) 
-# os. putenv ( 'DOCKER_PASSWORD' , 'password' ) 
-# os. putenv ( 'DOCKER_EMAIL' , 'none@example.org' ) 
-# os. putenv ( 'DOCKER_REGISTRY' , 'registry.example.org' ) 
+# os. putenv ( 'DOCKER_USERNAME' , 'registry-user' )
+# os. putenv ( 'DOCKER_PASSWORD' , 'password' )
+# os. putenv ( 'DOCKER_EMAIL' , 'none@example.org' )
+# os. putenv ( 'DOCKER_REGISTRY' , 'registry.example.org' )
 # Namespace='sandbox-hcfp'
 # os.putenv('NAMESPACE',Namespace)
 # allow_k8s_contexts('kubernetes-admin')
 # k8s_yaml(helm('./k8s/helm/hcfmailerplus', name='hcfmailer-plus', namespace=Namespace, values='dev-values.yaml'))
 # custom_build('highcanfly/hcfmailer-plus','./kaniko-build.sh',['./autocert', './scripts'],skips_local_docker=True)
-
 
 #kubectl create secret generic registry-credentials --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
 #   EXPECTED_REF=serveur/highcanfly_hcfmailer-plus:tilt-build-1683738819
@@ -19,10 +18,10 @@
 #   EXPECTED_REGISTRY=server.fqdn
 kubectl create namespace $NAMESPACE
 kubectl create secret -n $NAMESPACE docker-registry registry-credentials --docker-server=$EXPECTED_REGISTRY --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
-tar -cv --exclude "node_modules" --exclude ".git" --exclude ".github" --exclude-vcs --exclude ".docker" -f -  . | gzip -9 | kubectl run -n $NAMESPACE kaniko \
-    --rm --stdin=true \
-    --image=gcr.io/kaniko-project/executor:latest --restart=Never \
-    --overrides='{
+tar -cv --exclude "node_modules" --exclude "dkim.rsa" --exclude "private" --exclude "k8s" --exclude ".git" --exclude ".github" --exclude-vcs --exclude ".docker" --exclude "_sensitive_datas" -f - . | gzip -9 | kubectl run -n $NAMESPACE kaniko \
+  --rm --stdin=true \
+  --image=gcr.io/kaniko-project/executor:latest --restart=Never \
+  --overrides='{
   "apiVersion": "v1",
   "spec": {
     "containers": [
@@ -33,6 +32,9 @@ tar -cv --exclude "node_modules" --exclude ".git" --exclude ".github" --exclude-
         "stdinOnce": true,
         "args": [
           "-v","info",
+          "--cache=true",
+          "--compressed-caching=true",
+          "--cache-dir=/cache",
           "--dockerfile=Dockerfile",
           "--context=tar://stdin",
           "--skip-tls-verify",
@@ -42,6 +44,10 @@ tar -cv --exclude "node_modules" --exclude ".git" --exclude ".github" --exclude-
           {
             "name": "kaniko-secret",
             "mountPath": "/kaniko/.docker"
+          },
+          {
+            "name": "kaniko-cache",
+            "mountPath": "/cache"
           }
         ]
       }
@@ -58,6 +64,11 @@ tar -cv --exclude "node_modules" --exclude ".git" --exclude ".github" --exclude-
             }
           ]
         }
+      },
+      {
+        "name": "kaniko-cache",
+        "persistentVolumeClaim":
+            {"claimName": "kaniko-cache"}
       }
     ],
     "restartPolicy": "Never"
